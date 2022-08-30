54994e39c5138588b50b94309fe167aab97e621b
Antonio Di Meglio <82523820+antonio-dimeglio@users.noreply.github.com>
2022-08-30 19:07:49 +0200

    Implemented external programs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import json
 import os
 from termcolor import colored
+import importlib
+from multiprocessing import Process
+
 
 #Constants
 EXIT_SUCCESS = 0 
@@ -8,7 +11,7 @@ EXIT_FAILURE = -1
 BUILTINS = ["ls", "cd", "help", "exit"]
 
 #Global variables 
-configs = {}
+programs = {}
 current_dir = ""
 current_status = 0
 
@@ -29,8 +32,11 @@ def listing():
 
 def change_dir(path:str):
     try:
+        current_status = EXIT_FAILURE
         os.chdir(path)
         current_dir = os.getcwd()
+        current_status = EXIT_SUCCESS
+
     except FileNotFoundError:
         print("> " + colored(f"Could not find folder at path {path}", "red"))
     except PermissionError:
@@ -47,15 +53,19 @@ def load_config() -> dict:
     # The config file is used to map all (implemented) tools 
     # to a precise .py file and to also set up some variables
     # such as current_dir
+    # The map.json file contains as a key the python script name 
+    # and as a value its entry point i.e. a function
+    global programs 
     with open('./map.json') as f:
         data = json.load(f)
-    configs = data
+    programs = data
 
     current_dir = os.getcwd()
 
 def parse_line(line:str) -> list[str]:
     # This function is used to perform a very basic line parsing to split 
     # all the args onto a list 
+
     line = line.replace('\n', '')
     line = line.replace('\r', '')
     line = line.replace('\t', ' ')
@@ -63,8 +73,17 @@ def parse_line(line:str) -> list[str]:
     args = line.split(' ')
     return args 
 
+def check_external(program_name:str):
+    return program_name in programs.keys()
+
 def execute(args:list[str]) -> int:
-    
+    # This function is used to execute the program/command the user is calling
+    # If the command is part of the builtin functions then the command is executed directly
+    # Otherwise we lookup in the dictionary containing all implemented scripts if the program
+    # called exists, if it is found a process is spawned to run it, otherwise a prompt tells
+    # the user that such program has not been found.
+
+
     match args[0]:
 
         case "ls":
@@ -76,11 +95,17 @@ def execute(args:list[str]) -> int:
         case "exit":
             terminate()
         case _:
-            print("TODO")
+            if check_external(program_name=args[0]):
+                module = importlib.import_module(".", f"tools.{args[0]}")
+                entry_point = getattr(module, programs[args[0]])
+                p = Process(target=entry_point, args=args[1:])
+                p.run()
+            else:
+                print(">", colored(f"Could not find script for program {args[0]}", "red"))
 
 
 def terminate():
-    print(f"Terminated with status {current_status}")
+    print(f"Terminated with code {current_status}")
     exit(current_status)
 
 def command_loop():
---
+++ src/tools/prcs.py
@@ -0,0 +1,2 @@
+def main(args:list[str]):
+    print("hello")
